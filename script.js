9fb185386a0f06efd26a1cd788de9f750b7e788a
томихсм <tomitoalbornoz@gmail.com>
2026-09-22 09:41:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let Temperatura = prompt("Ingrese la temperatura en grados Celsius:");
+
+
+if (Temperatura <15) {
+   console.log("La temperatura puede ser baja, ten cuidado con el frío!");
+    alert("La temperatura puede ser baja, ten cuidado con el frío!");
+
+
+}else if (Temperatura >25) {
+    console.log("La temperatura puede ser alta, ten cuidado con el calor!");
+    alert( "La temperatura puede ser alta, ten cuidado con el calor!");
+
+
+}else{
+    console.log("La temperatura es moderada, disfruta del clima!");
+    alert("La temperatura es moderada, disfruta del clima!");
+}
